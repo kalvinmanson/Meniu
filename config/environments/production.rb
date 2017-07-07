5821e5e0b92d17c428b6298b176c6df222335649
Kalvin Manson <kalvinmanson@gmail.com>
2017-07-07 12:42:50 -0500

    ajust sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { :host => "meniudeldia.com" }
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true 
+  config.action_mailer.delivery_method = :sendmail #:smtp
 end
