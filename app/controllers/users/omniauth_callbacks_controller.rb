74aa7f0ade2cb9476892408f2822933cb659244a
Kalvin Manson <kalvinmanson@gmail.com>
2017-08-30 23:47:40 -0500

    Map presition

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -30,10 +30,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     flash[:notice] = "failure"
     redirect_to root_path
   end
-
-  protected
-
-  def update_resource(resource, params)
-    resource.update_without_password(params)
-  end
 end
